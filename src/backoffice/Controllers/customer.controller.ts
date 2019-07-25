97998d1ea5b3359802fad1cdc3b700074f0b8620
Thiago Coelho <thiag0coelho@outlook.com>
2019-07-25 01:11:30 -0300

    Creating customer service

--- src/backoffice/Controllers/customer.controller.ts
+++ src/backoffice/Controllers/customer.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Body,
   UseInterceptors,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { Result } from '../models/result.model';
 import { ValidatorInterceptor } from '../../interceptors/validator.interceptor';
@@ -14,10 +16,15 @@ import { CreateCustomerContract } from '../contracts/customer.contracts';
 import { CreateCustomerDto } from '../dtos/create-customer-dto';
 import { AccountService } from '../services/account.service';
 import { User } from '../models/user.model';
+import { CustomerService } from '../services/customer.service';
+import { Customer } from '../models/customer.model';
 
 @Controller('v1/customers')
 export class CustomerController {
-  constructor(private readonly accountService: AccountService) {}
+  constructor(
+    private readonly accountService: AccountService,
+    private readonly customerService: CustomerService,
+  ) {}
 
   @Get()
   get() {
@@ -32,11 +39,30 @@ export class CustomerController {
   @Post()
   @UseInterceptors(new ValidatorInterceptor(new CreateCustomerContract()))
   async post(@Body() model: CreateCustomerDto) {
-    const user = await this.accountService.create(
-      new User(model.document, model.password, true),
-    );
+    try {
+      const user = await this.accountService.create(
+        new User(model.document, model.password, true),
+      );
 
-    return new Result('Client created', true, user, null);
+      const customer = new Customer(
+        model.name,
+        model.document,
+        model.email,
+        null,
+        null,
+        null,
+        null,
+        user,
+      );
+      const res = await this.customerService.create(customer);
+
+      return new Result('Client created', true, res, null);
+    } catch (error) {
+      throw new HttpException(
+        new Result('Something went wrong', false, null, error),
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
   @Put(':document')
---
+++ src/backoffice/services/customer.service.ts
@@ -0,0 +1,15 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { Customer } from '../Models/customer.model';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class CustomerService {
+  constructor(@InjectModel('Customer') private readonly model: Model<Customer>) {}
+
+  async create(data: Customer): Promise<Customer> {
+    const customer = new this.model(data);
+
+    return await customer.save();
+  }
+}
